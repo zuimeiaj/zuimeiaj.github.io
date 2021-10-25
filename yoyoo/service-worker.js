ac267ede0eac7fd50d483d64ad3c607c338ceeb3
Jun Yao <2498683974@qq.com>
2021-10-25 16:08:20 +0800

    update presets

--- yoyoo/service-worker.js
+++ yoyoo/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yoyoo/precache-manifest.902c06ea6ce5b33bf85dbf0d9438cd88.js"
+  "/precache-manifest.c55b5090cf0402fc07f5ad9ce630cc20.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yoyoo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
